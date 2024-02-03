d39f92d43bf55febee6d0b30c6f78fb1db1d8353
Harsh Sharma <harshsharma0828@gmail.com>
2024-02-03 12:41:35 +0530

    fixed a minor bug in constant.js file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,13 @@
-// require('dotenv').config()
+
 
 
 export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer' + process.env.REACT_APP_API_TOKEN,
+    Authorization: 'Bearer ' + process.env.REACT_APP_API_TOKEN,
   }
 };
+
 
 export const IMG_CDN = "https://image.tmdb.org/t/p/w300";
